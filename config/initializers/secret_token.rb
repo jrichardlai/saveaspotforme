e6a2454e332191596c431ef2cd6848d67d249ffc
jrichardlai <jrichardlai@gmail.com>
2012-08-08 00:48:53 -0700

    Remove secret_token from initializers.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Saveaspotforme::Application.config.secret_token = 'ee152350a86ef169bd633fd0d4cbcfa4b4c0d040413d37a8e95363e13c3764f24a4c7a5bc22a037192d758fe038f1beb61e65f2bf1fd4e8961dbbdb4cf4d9d73'
+Saveaspotforme::Application.config.secret_token = ENV['SECRET_TOKEN']
