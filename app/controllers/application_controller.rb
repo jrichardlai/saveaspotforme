96b586cf71ab1cefbee42d2323ac9d6829e03ff6
jrichardlai <jrichardlai@gmail.com>
2012-08-07 22:57:52 -0700

    Use try for session in ApplicationController.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,21 +2,15 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   before_filter :current_user
-  before_filter :ensure_session
-
   helper_method :current_user
 
   def current_user
-    @current_user ||= User.where(id: session[:current_user_id]).first
+    @current_user ||= User.where(id: session.try(:[], :current_user_id)).first
   end
 
   protected
 
   def require_login
     redirect_to(root_path, {:error => "Need to be logged in"}) unless current_user
-  end
-
-  def ensure_session
-    session ||= {}
   end
 end
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Saveaspotforme::Application.config.session_store :cookie_store, key: '_saveaspotforme_session'
+Saveaspotforme::Application.config.session_store :cookie_store, key: '_saveaspotforme_session', httponly: false
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
