13cd495f234d086a7c11018308c68ccf68526f09
jrichardlai <jrichardlai@gmail.com>
2012-08-07 22:48:27 -0700

    Ensure session is set.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
   before_filter :current_user
+  before_filter :ensure_session
+
   helper_method :current_user
 
   def current_user
@@ -11,5 +14,9 @@ class ApplicationController < ActionController::Base
 
   def require_login
     redirect_to(root_path, {:error => "Need to be logged in"}) unless current_user
+  end
+
+  def ensure_session
+    session ||= {}
   end
 end
